8d986ee2d1f066d5b33d6fea318609241fb189c1
imceballos <imceballos@uc.cl>
2020-03-17 19:23:34 -0300

    Add a .gitignore with favorits

--- src/Movieinfo.js
+++ src/Movieinfo.js
@@ -21,9 +21,9 @@ const MovieInfo = (props) => {
 			<div className="row">
 				<div className="col s12 m4">
 					{
-						props.currentMovie.poster_path == null ? <img src= {"https://s3-ap-southeast-1.amazonaws.com/upcode/static/default-image.jpg"} alt="Card image" style = {{width : "100%" , height :360}}/> :
+						props.currentMovie.poster_path == null ? <img src= {"https://s3-ap-southeast-1.amazonaws.com/upcode/static/default-image.jpg"} alt="Card image" style = {{width : "100%" , height :360}} /> :
 
-						<img src={"https://image.tmdb.org/t/p/w185"+props.currentMovie.poster_path} alt="Card image" style = {{width : "100%" , height :360}}/>
+						<img src={"https://image.tmdb.org/t/p/w185"+props.currentMovie.poster_path}  alt="Card image" style = {{width : "100%" , height :360}} />
 
 					}
 				</div>
