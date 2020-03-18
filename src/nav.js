0813736446906f057b782232731f107a526d8259
imceballos <48004620+imceballos@users.noreply.github.com>
2020-03-18 10:44:25 -0300

    Update nav.js

--- src/nav.js
+++ src/nav.js
@@ -11,7 +11,7 @@ const Nav = (props) => {
 	return(
 		<nav className="nav-extended">
 			<div className = "nav-wrapper">
-				<a  href="/" className="brand-logo" >	<Link to = "/"><i className="material-icons"></i> </Link>Movie finder </a> 
+				<a  href="https://imceballos.github.io/react-app-movie/" className="brand-logo" >	<Link to = "/"><i className="material-icons"></i> </Link>Movie finder </a> 
 				<ul id="nav-mobile" className="right hide-on-med-and-down" >
 					<Link to="/favoritos" ><li onClick={ props.closeMovieInfo }  > Favoritos </li> </Link>
 
