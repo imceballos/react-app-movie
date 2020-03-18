1271e8c2cd44468a03ce9d0d738b2a19b763005d
imceballos <imceballos@uc.cl>
2020-03-18 12:05:55 -0300

    Updates

--- precache-manifest.5767c617d6ce1448c8b214426e8e3d01.js
+++ precache-manifest.5767c617d6ce1448c8b214426e8e3d01.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "68625d9c0de7e11aca4da5fd358ee584",
+    "revision": "a4541b4870604254f92a6e79cc263c60",
     "url": "/react-app-movie/index.html"
   },
   {
-    "revision": "6ff1e8274d906df3aa41",
+    "revision": "ca85e7b778a3c505cd30",
     "url": "/react-app-movie/static/css/main.62036572.chunk.css"
   },
   {
-    "revision": "c57963f2f12ea0bb4fcf",
-    "url": "/react-app-movie/static/js/2.6a453f34.chunk.js"
+    "revision": "bf66d6c1317023444336",
+    "url": "/react-app-movie/static/js/2.3dfc67c5.chunk.js"
   },
   {
     "revision": "f231859d6585c4cd5f80c344783ed269",
-    "url": "/react-app-movie/static/js/2.6a453f34.chunk.js.LICENSE.txt"
+    "url": "/react-app-movie/static/js/2.3dfc67c5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6ff1e8274d906df3aa41",
-    "url": "/react-app-movie/static/js/main.7b1628ea.chunk.js"
+    "revision": "ca85e7b778a3c505cd30",
+    "url": "/react-app-movie/static/js/main.048d8e48.chunk.js"
   },
   {
     "revision": "8be930f50a41a44e5569",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-app-movie/precache-manifest.9bb9d5457c96b5edaef7ad9ed2b271be.js"
+  "/react-app-movie/precache-manifest.5767c617d6ce1448c8b214426e8e3d01.js"
 );
 
 self.addEventListener('message', (event) => {
