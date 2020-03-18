c87fed2d56f5eef8701b45fcca81fd36a39c91e3
imceballos <imceballos@uc.cl>
2020-03-18 10:37:55 -0300

    version online 1.0.1.1.8 revert

--- src/nav.js
+++ src/nav.js
@@ -11,11 +11,9 @@ const Nav = (props) => {
 	return(
 		<nav className="nav-extended">
 			<div className = "nav-wrapper">
-				<a  href="/" className="brand-logo" >	<Link to = "/"><i className="material-icons"></i> </Link>Movie finder </a> 
+				<a  href="/#" className="brand-logo" >	<Link to = "/"><i className="material-icons"></i> </Link>Movie finder </a> 
 				<ul id="nav-mobile" className="right hide-on-med-and-down" >
 					<Link to="/favoritos" ><li onClick={ props.closeMovieInfo }  > Favoritos </li> </Link>
-
-
       			</ul>
 			</div>
 		</nav>
