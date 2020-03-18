8d2f74468f4cac3e04ea49fcd17970c3ba39a3dd
imceballos <imceballos@uc.cl>
2020-03-17 22:41:00 -0300

    Updates

---
+++ precache-manifest.df0f49c1cab6671c1abc8fc3d38b470b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e52289851f98eacbc6b0db87def6d490",
+    "url": "/react-app-movie/index.html"
+  },
+  {
+    "revision": "9ec8231ccb0cf086ecae",
+    "url": "/react-app-movie/static/css/main.5c7f316a.chunk.css"
+  },
+  {
+    "revision": "9d3b56a0d979d72d89e1",
+    "url": "/react-app-movie/static/js/2.1aaac1e4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-app-movie/static/js/2.1aaac1e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ec8231ccb0cf086ecae",
+    "url": "/react-app-movie/static/js/main.1020ad44.chunk.js"
+  },
+  {
+    "revision": "8be930f50a41a44e5569",
+    "url": "/react-app-movie/static/js/runtime-main.75e1ac7c.js"
+  }
+]);
